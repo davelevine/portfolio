2c156cbd776af302b9f506c7aea232b988df3087
davelevine <dave@davelevine.io>
2023-10-25 13:13:27 -0400

    Minor styling changes for About section

--- components/about/about.js
+++ components/about/about.js
@@ -18,11 +18,12 @@ const About = () => {
           <div className={classes.columnLeft} data-aos='fade-right'>
             <h1>Designing effective solutions, organizing knowledge, and conveying complexity through visual design.</h1>
             <br />
-            <p className={classes.paragraph}>
-              <h4 style={{ fontWeight: '400' }}>I&apos;m an information systems pro with a knack for building relationships and technical expertise to make great things happen for businesses. I&apos;m experienced in leading diverse teams, and together, we&apos;ve tackled the nitty-gritty of business processes, amped up the tech stuff, ensured top-notch quality, and made web-based solutions shine.</h4>
+            <p className={classes.h4}>
+              I&apos;m an information systems pro with a knack for building relationships and technical expertise to make great things happen for businesses. I&apos;m experienced in leading diverse teams, and together, we&apos;ve tackled the nitty-gritty of business processes, amped up the tech stuff, ensured top-notch quality, and made web-based solutions shine.
             </p>
-            <p className={classes.paragraph}>
-              <h4 style={{ fontWeight: '400' }}>Most recently, I work closely with the Solutions Architect to shape application architecture, fine-tune development processes, and participate in solution design discussions to meet business needs.</h4>
+            <br />
+            <p className={classes.h4}>
+              Most recently, I work closely with the Solutions Architect to shape application architecture, fine-tune development processes, and participate in solution design discussions to meet business needs.
             </p>
           </div>
 
@@ -36,8 +37,7 @@ const About = () => {
                 data-aos='fade-left'
               />
             </div>
-            <div className={classes.quote} data-aos='fade-right'>
-            </div>
+            <div className={classes.quote} data-aos='fade-right'></div>
           </div>
         </div>
       </div>
