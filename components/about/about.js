c554134191c7f30e03f66d6870abd99ab732d02c
davelevine <dave@davelevine.io>
2023-10-24 23:37:04 -0400

    Fixed errors in About section

--- components/about/about.js
+++ components/about/about.js
@@ -16,14 +16,12 @@ const About = () => {
 
         <div className={classes.row}>
           <div className={classes.columnLeft} data-aos='fade-right'>
-            <p>
-              <h1>Designing effective solutions, organizing knowledge, and conveying complexity through visual design.</h1>
-            <p/>
-            <br />
-              I&apos;m an information systems pro with a knack for building relationships and technical expertise to make great things happen for businesses. I&apos;m experienced in leading diverse teams and together, we&apos;ve tackled the nitty-gritty of business processes, amped up the tech stuff, ensured top-notch quality, and made web-based solutions shine.
+            <h1>Designing effective solutions, organizing knowledge, and conveying complexity through visual design.</h1>
+            <p className={classes.paragraph}>
+              I&apos;m an information systems pro with a knack for building relationships and technical expertise to make great things happen for businesses. I&apos;m experienced in leading diverse teams, and together, we&apos;ve tackled the nitty-gritty of business processes, amped up the tech stuff, ensured top-notch quality, and made web-based solutions shine.
             </p>
             <br />
-            <p>
+            <p className={classes.paragraph}>
               Most recently, I work closely with the Solutions Architect to shape application architecture, fine-tune development processes, and participate in solution design discussions to meet business needs.
             </p>
           </div>
