4772ec7fb15cfb46527ba05edc7c6f239077199a
davelevine <dave@davelevine.io>
2023-10-29 11:11:34 -0400

    Updated favicon

--- pages/_app.js
+++ pages/_app.js
@@ -24,14 +24,14 @@ function MyApp({ Component, pageProps }) {
               name='viewport'
               content='width=device-width, initial-scale=1'
             />
-            <link rel='shortcut icon' href='/favicon/favicon.ico' />
+            <link rel='shortcut icon' href='images/favicon/favicon.ico' />
 
             {/* Add links for different favicon sizes here */}
-            <link rel="icon" type="image/png" sizes="192x192" href="/favicon/android-chrome-192x192.png" />
-            <link rel="icon" type="image/png" sizes="512x512" href="/favicon/android-chrome-512x512.png" />
-            <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
-            <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
-            <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+            <link rel="icon" type="image/png" sizes="192x192" href="images/favicon/android-chrome-192x192.png" />
+            <link rel="icon" type="image/png" sizes="512x512" href="images/favicon/android-chrome-512x512.png" />
+            <link rel="icon" type="image/png" sizes="16x16" href="images/favicon/favicon-16x16.png" />
+            <link rel="icon" type="image/png" sizes="32x32" href="images/favicon/favicon-32x32.png" />
+            <link rel="apple-touch-icon" sizes="180x180" href="images/favicon/apple-touch-icon.png" />
           </Head>
           <Component {...pageProps} currentTheme={theme} />
           <Footer />
