24247c49b67c414577dc3a6de76eaadb735e2227
davelevine <dave@davelevine.io>
2023-10-31 12:00:43 -0400

    Changed image loader from Akamai to next/images; updated image relative path in project sections; added Brotli compression

--- next.config.js
+++ next.config.js
@@ -1,12 +1,22 @@
 /** @type {import('next').NextConfig} */
+const withPlugins = require('next-compose-plugins');
+const withImages = require('next-images');
+
 const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    loader: 'akamai',
-    path: isProd ? '' : '',
-  },
   basePath: '',
 };
 
-module.exports = nextConfig;
+module.exports = withPlugins(
+  [
+    withImages, // Enable the next-images plugin if not already enabled
+    {
+      // Add Brotli compression to Next.js
+      compress: true,
+      poweredByHeader: false,
+    },
+  ],
+  nextConfig
+);
