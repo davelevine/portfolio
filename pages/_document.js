550f9178e7ef2902bc6c0988b70f4efc0f1b0a5f
davelevine <dave@davelevine.io>
2023-10-31 21:07:49 -0400

    Fixed font location issue from 87f2406

--- pages/_document.js
+++ pages/_document.js
@@ -11,30 +11,13 @@ class MyDocument extends Document {
             href='/fonts/googlefonts/google-fonts.css' // Path to locally hosted Google Fonts CSS
           />
 
-          {/* Link to locally hosted FontAwesome CSS */}
+          {/* Link to locally hosted FontAwesome CSS (all.min.css) */}
           <link
             rel='stylesheet'
-            href='/fonts/all.min.css' // Path to locally hosted FontAwesome CSS
+            href='/fonts/all.min.css' // Adjust the path as needed
           />
 
-          <style>{`
-            @font-face {
-              font-family: "Font Awesome 6 Free";
-              src: url('/fonts/webfonts/fa-solid-900.woff2') format('woff2'),
-                   url('/fonts/webfonts/fa-solid-900.ttf') format('truetype');
-              font-weight: 900;
-              font-style: normal;
-            }
-
-            @font-face {
-              font-family: "Font Awesome 6 Brands";
-              src: url('/webfonts/fa-brands-400.woff2') format('woff2'),
-                   url('/webfonts/fa-brands-400.ttf') format('truetype');
-              font-weight: 400;
-              font-style: normal;
-            }
-          `}
-          </style>
+          {/* No need for custom @font-face declarations for Font Awesome */}
           {/* Plausible tracking script */}
           <script defer data-domain="www.davelevine.io" data-api="/data/api/event" src="/data/js/script.js"></script>
         </Head>
