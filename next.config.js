fd838527e857b33611ef684cc6e5f34f04380dd1
davelevine <dave@davelevine.io>
2023-11-05 08:40:42 -0500

    Added JS and CSS minification to next.config.js; compressed favicons and multiple images throughout project

--- next.config.js
+++ next.config.js
@@ -1,9 +1,20 @@
 /** @type {import('next').NextConfig} */
 const withPlugins = require('next-compose-plugins');
+const TerserPlugin = require('terser-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
 const nextConfig = {
   reactStrictMode: true,
   basePath: '',
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.optimization.minimizer.push(new TerserPlugin());
+    }
+
+    config.optimization.minimizer.push(new CssMinimizerPlugin());
+
+    return config;
+  },
 };
 
 module.exports = withPlugins(
