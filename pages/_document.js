38e15424f44555d00c090cbe71c1fe91725691eb
davelevine <dave@davelevine.io>
2023-11-07 10:12:15 -0500

    Modified _document.js to load CSS asynchronously

--- pages/_document.js
+++ pages/_document.js
@@ -5,27 +5,8 @@ class MyDocument extends Document {
     return (
       <Html lang='en'>
         <Head>
-          {/* Link to locally hosted Google Fonts CSS */}
-          <link
-            rel='stylesheet'
-            href='/fonts/googlefonts/google-fonts.css'
-          />
-
-          {/* Link to CDN hosted FontAwesome CSS */}
-          <link
-            rel="stylesheet"
-            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous"
-            referrerpolicy="no-referrer"
-          />
-
           {/* Reference to manifest.json */}
           <link rel="manifest" href="/manifest.json" />
-
-          {/* No need for custom @font-face declarations for Font Awesome */}
-          {/* Plausible tracking script */}
-          <script defer data-domain="www.davelevine.io" data-api="/data/api/event" src="/data/js/script.js"></script>
-
-          <link rel="shortcut icon" href="/images/favicon/favicon.ico" />
 
           {/* Add links for different favicon sizes here */}
           <link rel="icon" type="image/png" sizes="192x192" href="/images/favicon/android-chrome-192x192.png" />
@@ -33,6 +14,28 @@ class MyDocument extends Document {
           <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon/favicon-16x16.png" />
           <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon/favicon-32x32.png" />
           <link rel="apple-touch-icon" sizes="180x180" href="/images/favicon/apple-touch-icon.png" />
+          
+          {/* Plausible tracking script */}
+          <script defer data-domain="www.davelevine.io" data-api="/data/api/event" src="/data/js/script.js"></script>
+
+          {/* Dynamically load CSS asynchronously */}
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                function loadCSS(href) {
+                  var link = document.createElement('link');
+                  link.rel = 'stylesheet';
+                  link.href = href;
+                  link.type = 'text/css';
+                  link.media = 'all';
+                  document.head.appendChild(link);
+                }
+
+                loadCSS('/fonts/googlefonts/google-fonts.css');
+                loadCSS('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css');
+              `,
+            }}
+          />
         </Head>
         <body>
           <Main />
