b4912c9e0e97cf82e197b222f9e288d9752270c9
davelevine <dave@davelevine.io>
2023-11-06 22:13:58 -0500

    Consolodated and reorganized vercel.json; reduced redundancy in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 // Import necessary packages
+import pkg from 'next-compose-plugins';
 import withPWA from 'next-pwa';
 import CompressionPlugin from 'compression-webpack-plugin';
-import pkg from 'next-compose-plugins';
 import TerserPlugin from 'terser-webpack-plugin';
 import CssMinimizerPlugin from 'css-minimizer-webpack-plugin';
 
@@ -58,47 +58,49 @@ const runtimeCaching = [
   },
 ];
 
+// Webpack configuration
+const configureWebpack = (config, { isServer }) => {
+  if (!isServer) {
+    config.optimization.minimizer.push(new TerserPlugin());
+  }
+  config.optimization.minimizer.push(new CssMinimizerPlugin());
+
+  // Configure Gzip compression for assets
+  config.plugins.push(
+    new CompressionPlugin({
+      test: /\.(js|css|html|svg)$/,
+    })
+  );
+
+  return config;
+};
+
+// PWA configuration
+const pwaOptions = {
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: !isProduction,
+  runtimeCaching,
+  scope: '/',
+  dynamicStartUrl: true,
+  reloadOnOnline: false,
+  // Add other options as needed
+};
+
+// Brotli compression configuration
+const brotliOptions = {
+  compress: true,
+  poweredByHeader: false,
+};
+
 const nextConfig = {
   reactStrictMode: true,
   basePath: '',
   experimental: {
     webpackBuildWorker: true,
   },
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.optimization.minimizer.push(new TerserPlugin());
-    }
-    config.optimization.minimizer.push(new CssMinimizerPlugin());
-
-    // Configure Gzip compression for assets
-    config.plugins.push(
-      new CompressionPlugin({
-        test: /\.(js|css|html|svg)$/,
-      })
-    );
-
-    return config;
-  },
+  webpack: configureWebpack,
 };
 
-export default pkg(
-  [
-    withPWA({
-      dest: 'public',
-      register: true,
-      skipWaiting: true,
-      disable: !isProduction,
-      runtimeCaching, // Use the runtimeCaching configuration defined above
-      scope: '/', // Customize PWA scope if needed
-      dynamicStartUrl: true, // If your start URL varies based on user state
-      reloadOnOnline: false, // Decide how the app refreshes when online
-      // Add other options as needed
-    }),
-    {
-      // Add Brotli compression
-      compress: true,
-      poweredByHeader: false,
-    },
-  ],
-  nextConfig
-);
+export default pkg([withPWA(pwaOptions), brotliOptions], nextConfig);
