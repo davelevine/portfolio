cae6a9384ecbc560b8424164fdd1fb417161a0a5
davelevine <dave@davelevine.io>
2024-07-03 11:02:29 -0400

    feat: Enhance hero and modal components with improved animations and styles
    - Updated `hero.js` to include framer-motion animations and AOS scroll effects.
    - Modified `modal.js` to improve modal visibility and added transition effects.
    - Enhanced `modal.module.scss` with new styles for better UI/UX.
    - Adjusted scrollbar handling in `hero.js` for better modal experience.
    - Improved responsiveness and accessibility across components.

--- components/home/hero.js
+++ components/home/hero.js
@@ -1,35 +1,41 @@
-// hero.js
-import { useState, useCallback, useEffect } from 'react';
-import Image from 'next/legacy/image';
-import { motion, AnimatePresence } from 'framer-motion';
 import classes from './hero.module.scss';
+import Image from 'next/legacy/image';
+import { useState, useEffect, useCallback } from 'react';
 import Modal from '../layout/modal/modal';
-import Aos from 'aos';
-import 'aos/dist/aos.css';
+import Aos from 'aos'; // Library for scroll animations
+import 'aos/dist/aos.css'; // Styles for AOS animations
+import { motion, AnimatePresence } from 'framer-motion';
 
+// Constants
 const RESUME_FILE_PATH = '/assets/davelevine-resume.pdf';
 const MOBILE_PROFILE_IMAGE_PATH = '/images/profile-pic-1-mobile.webp';
 const DESKTOP_PROFILE_IMAGE_PATH = '/images/profile-pic-1-desktop.webp';
+const PGP_KEY_PATH = '/assets/dave.levine.io-pgp-key-pub.asc'; // Path to the PGP key
 
 const Hero = () => {
+  // State for controlling the modal visibility
   const [showModal, setShowModal] = useState(false);
-  const [modalType, setModalType] = useState('');
-  const [isMobile, setIsMobile] = useState(false);
+  const [modalType, setModalType] = useState(''); // State to determine modal type (contact or resume)
+  const [isMobile, setIsMobile] = useState(false); // State to determine mobile/desktop
 
+  // Refactored button handlers into a single function
   const handleButtonClick = useCallback((path) => {
     window.location.href = path;
   }, []);
 
+  // Function to open the modal
   const showModalHandler = useCallback((type) => {
     setModalType(type);
     setShowModal(true);
   }, []);
 
+  // Function to close the modal
   const closeModalHandler = useCallback(() => {
     setShowModal(false);
     setModalType('');
   }, []);
 
+  // Effects for managing body overflow when the modal is open or closed
   useEffect(() => {
     const calculateScrollbarWidth = () => {
       const scrollDiv = document.createElement('div');
@@ -55,19 +61,25 @@ const Hero = () => {
     }
   }, [showModal]);
 
+  // Initialize the AOS library with specified settings
   useEffect(() => {
-    Aos.init({ duration: 550 });
+    Aos.init({ duration: 550 }); // Adjusted scroll animations speed to match about.js
   }, []);
 
+  // Detect if it's a mobile device on the client side
   useEffect(() => {
     if (typeof window !== 'undefined') {
       const updateIsMobile = () => {
         setIsMobile(window.innerWidth <= 767);
       };
 
+      // Initial check
       updateIsMobile();
+
+      // Update the state when the window is resized
       window.addEventListener('resize', updateIsMobile);
 
+      // Cleanup the event listener
       return () => {
         window.removeEventListener('resize', updateIsMobile);
       };
@@ -88,33 +100,53 @@ const Hero = () => {
       <div className={classes.container}>
         <div className={classes.row}>
           <div className={classes.columnLeft}>
-            <h2 data-aos='fade-left'>Hey, I&apos;m <span className={classes.name}>Dave</span>! </h2>
+            {/* Introduction section with data-aos animation attributes */}
+            <h2 data-aos='fade-left'>Hey, I&apos;m{' '}
+              <span className={classes.name}>
+                Dave
+              </span>!{' '}
+            </h2>
             <h1 data-aos='fade-right'>Solutions Engineer</h1>
             <h3 data-aos='fade-left' data-aos-delay='200'>
-              I design and implement systems<span className={classes.punctuation}><b>.</b></span>
+              I design and implement systems
+              <span className={classes.punctuation}><b>.</b></span>
+              <span>
+              </span>{' '}
             </h3>
             <div className={classes.socialMedia}>
+              {/* Links to social media profiles with data-aos animations */}
               <a href='https://github.com/davelevine' target='_blank' rel='noreferrer' aria-label="GitHub Profile">
                 <i className='fab fa-github' data-aos='flip-up'></i>
               </a>
               <a href='https://www.linkedin.com/in/iamdavelevine' target='_blank' rel='noreferrer' aria-label="LinkedIn Profile">
-                <i className='fab fa-linkedin' data-aos='flip-up' data-aos-delay='100'></i>
-              </a>
+                <i
+                  className='fab fa-linkedin'
+                  data-aos='flip-up'
+                  data-aos-delay='100'></i>
+              </a>{' '}
               <a href='https://kb.levine.io' target='_blank' rel='noreferrer' aria-label="Knowledge Base">
-                <i className='fas fa-globe' data-aos='flip-up' data-aos-delay='200'></i>
+                <i
+                  className='fas fa-globe'
+                  data-aos='flip-up'
+                  data-aos-delay='200'></i>
               </a>
-              <a href='/assets/dave.levine.io-pgp-key-pub.asc' target='_blank' rel='noreferrer' aria-label="PGP Key">
-                <i className='fa fa-key' data-aos='flip-up' data-aos-delay='300'></i>
+              <a href={PGP_KEY_PATH} target='_blank' rel='noreferrer' aria-label="PGP Key">
+                <i
+                  className='fa fa-key'
+                  data-aos='flip-up'
+                  data-aos-delay='300'></i>
               </a>
             </div>
             <div className={classes.ctaButtons}>
+              {/* Call-to-action buttons with framer-motion animations */}
               <motion.button
                 variants={buttonVariants}
                 className='btn btn-filled'
                 data-aos='fade-up'
                 onClick={() => showModalHandler('resume')}
                 aria-label="My Resume"
-                transition={{ duration: 0.55 }}>
+                transition={{ duration: 0.55 }} // Adjusted transition speed to match about.js
+              >
                 MY RESUME
               </motion.button>
               <motion.button
@@ -123,12 +155,15 @@ const Hero = () => {
                 data-aos='fade-down'
                 onClick={() => showModalHandler('contact')}
                 aria-label="Contact Me"
-                transition={{ duration: 0.55 }}>
+                transition={{ duration: 0.55 }} // Adjusted transition speed to match about.js
+              >
                 CONTACT
               </motion.button>
             </div>
           </div>
+
           <div className={`${classes.columnRight} ${classes.profilePic}`}>
+            {/* Conditionally render different images based on viewport width */}
             {isMobile ? (
               <Image
                 src={MOBILE_PROFILE_IMAGE_PATH}
@@ -156,10 +191,12 @@ const Hero = () => {
         </div>
       </div>
       <AnimatePresence>
+        {/* Display the modal when showModal is true */}
         {showModal && <Modal contact={modalType === 'contact'} resume={modalType === 'resume'} onClose={closeModalHandler} />}
       </AnimatePresence>
     </section>
   );
 };
 
+// Export the Hero component
 export default Hero;
--- components/layout/modal/modal.js
+++ components/layout/modal/modal.js
@@ -1,5 +1,5 @@
 // modal.js
-import React, { lazy, Suspense } from 'react';
+import React, { lazy, Suspense, useEffect } from 'react';
 import { motion } from 'framer-motion';
 import classes from './modal.module.scss';
 
@@ -7,34 +7,50 @@ import classes from './modal.module.scss';
 const ContactModalDynamic = lazy(() => import('./contactModal'));
 
 const Modal = ({ contact, resume, onClose, ...props }) => {
+  // Define animation variants for the modal
   const fadeIn = {
     hidden: { opacity: 0 },
     visible: { opacity: 1, transition: { duration: 0.3, type: 'easeInOut' } },
     exit: { opacity: 0 },
   };
 
-  const isMobile = () => window.innerWidth <= 767;
-
+  // Function to render the contact modal
   const renderContactModal = () => (
     <Suspense fallback={<p>Loading...</p>}>
       <ContactModalDynamic {...props} onClose={onClose} />
     </Suspense>
   );
 
-  const renderResumeModal = () => {
-    if (isMobile()) {
-      window.location.href = '/assets/davelevine-resume.pdf';
-      return null;
+  // Function to render the resume modal
+  const renderResumeModal = () => (
+    <div className={classes.resumeModal}>
+      <a href='#!' className={classes.close} onClick={onClose}>
+        <i className='fa fa-xmark'></i>
+      </a>
+      <iframe src='/assets/davelevine-resume.pdf' className={classes.iframe} />
+    </div>
+  );
+
+  // Handle click on the backdrop to close the modal
+  const handleBackdropClick = (e) => {
+    if (e.target === e.currentTarget) {
+      onClose();
     }
-    return (
-      <div className={classes.resumeModal}>
-        <a href='#!' className={classes.close} onClick={onClose}>
-          <i className='fa fa-xmark'></i>
-        </a>
-        <iframe src='/assets/davelevine-resume.pdf' className={classes.iframe} />
-      </div>
-    );
   };
+
+  // Effect to handle the Escape key press to close the modal
+  useEffect(() => {
+    const handleEscapeKey = (e) => {
+      if (e.key === 'Escape') {
+        onClose();
+      }
+    };
+
+    document.addEventListener('keydown', handleEscapeKey);
+    return () => {
+      document.removeEventListener('keydown', handleEscapeKey);
+    };
+  }, [onClose]);
 
   return (
     <motion.div
@@ -42,7 +58,7 @@ const Modal = ({ contact, resume, onClose, ...props }) => {
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
       className={classes.backdrop}
-      onClick={onClose} // Close modal when clicking the backdrop
+      onClick={handleBackdropClick} // Handle backdrop click
     >
       <motion.div
         onClick={(e) => e.stopPropagation()} // Prevent click propagation to backdrop
