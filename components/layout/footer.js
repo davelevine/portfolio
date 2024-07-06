3a455852d8bf59dd122eebd6dfcb0c661e27b63c
davelevine <dave@davelevine.io>
2024-07-05 20:45:14 -0400

    Re-added the footer with content removed and adjusted layout for improved spacing and alignment.
    Changes:
    - Re-added the footer but removed all the content.
    - Set the min-height to 90vh (from 100vh) for min-width 768px in .
    - Set the padding for the entire container to .
    - Set the viewport height to 90vh (from ).
    - Adjusted the container height to 110% (from 100%) to fit within the new hero size.
    
    Note: Unable to fully display contents in horizontal viewport on mobile devices, but acceptable partial display achieved.

--- components/layout/footer.js
+++ components/layout/footer.js
@@ -3,15 +3,6 @@ import classes from './footer.module.scss';
 const Footer = () => {
   return (
     <footer className={classes.footer}>
-      <div className={classes.socialMedia}>
-        <a href='https://github.com/davelevine' target='_blank' rel='noreferrer' aria-label="GitHub Profile">
-          <i className='fab fa-github'></i>
-        </a>
-        <a href='https://www.linkedin.com/in/iamdavelevine' target='_blank' rel='noreferrer' aria-label="LinkedIn Profile">
-          <i className='fab fa-linkedin'></i>
-        </a>
-      </div>
-      <div>© Dave Levine</div>
     </footer>
   );
 };
