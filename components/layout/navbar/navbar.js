davelevine/portfolio

commit de4537383b887fc0bcbbb43d9dff85094292c92d

Update hero and navbar components
- Minor changes to hero page for layout and styling improvements.
- Updated navbar components for upcoming Sass changes.
- Enhanced project content pages for improved project display.

components/layout/navbar/navbar.js
@@ -71,18 +71,18 @@ const Navbar = ({ theme, newTheme, children }) => {
     <>
       <div className={sticky ? `${classes.navbar} ${classes.sticky}` : classes.navbar}>
         <div className={classes.container}>
-          <Link href='/' passHref legacyBehavior>
-            <a className={classes.logo}>
+          <Link href='/' passHref>
+            <span className={classes.logo}>
               <span className={classes.name}>&lt;</span>/DAVE LEVINE
               <span className={classes.name}>&gt;</span>
-            </a>
+            </span>
           </Link>
 
           <nav className={isOpen ? `${classes.navMenu} ${classes.responsive}` : classes.navMenu} id='navMenu'>
             <div className={classes.linkWrapper}>
               {['/', '/projects', '/certs', '/about'].map((path, index) => (
-                <Link href={path} passHref legacyBehavior key={path}>
-                  <motion.a
+                <Link href={path} passHref key={path}>
+                  <motion.span
                     style={{ cursor: 'pointer' }}
                     initial={{ opacity: 0, y: -100 }}
                     animate={{ opacity: 1, y: 0 }}
@@ -91,7 +91,7 @@ const Navbar = ({ theme, newTheme, children }) => {
                     className={isLinkActive(path) ? classes.activeLink : ''}
                   >
                     {path.toUpperCase().replace('/', '') || 'HOME'}
-                  </motion.a>
+                  </motion.span>
                 </Link>
               ))}
             </div>

components/home/hero.js
@@ -139,7 +139,7 @@ const Hero = () => {
                     borderRadius: "50%",
                     objectFit: "contain"
                   }} 
-                  priority
+                  priority={false} // Remove priority to avoid preloading issue
                   placeholder="blur"
                   blurDataURL={PROFILE_PIC_BLUR_DATA_URL}
                 />

components/home/hero.module.scss
@@ -159,6 +159,8 @@
         img {
           border-radius: 50%;
           object-fit: cover;
+          width: 100%; /* Ensure the image takes the full width of the wrapper */
+          height: 100%; /* Ensure the image takes the full height of the wrapper */
         }
       }
 
@@ -242,6 +244,8 @@
           img {
             border-radius: 50%;
             object-fit: cover;
+            width: 100%; /* Ensure the image takes the full width of the wrapper */
+            height: 100%; /* Ensure the image takes the full height of the wrapper */
           }
         }
       }
@@ -301,6 +305,11 @@
         .imageWrapper {
           max-width: 500px;
           max-height: 500px;
+
+          img {
+            width: 100%; /* Ensure the image takes the full width of the wrapper */
+            height: 100%; /* Ensure the image takes the full height of the wrapper */
+          }
         }
       }
     }

components/layout/navbar/navbar.module.scss
@@ -5,6 +5,7 @@
   width: 100%;
   position: fixed; /* Fixed position at the top */
   transition: background 0.3s; /* Smooth background transition */
+  margin-bottom: 0; /* Move this declaration above nested rules */
 
   &.sticky {
     /* Styles for the sticky navigation bar */
@@ -37,12 +38,11 @@
     margin-right: auto; /* Adjust the logo's right margin */
     margin-left: 20px;
     white-space: nowrap; /* Prevent text wrapping */
+    transition: color 0.3s; /* Smooth color transition */
 
     .name {
       color: var(--background-secondary); /* Set the color for the brackets */
     }
-
-    transition: color 0.3s; /* Smooth color transition */
 
     &:hover {
       color: var(--text-secondary); /* Change color on hover */
@@ -132,11 +132,6 @@
       justify-content: center;
       align-items: center;
     }
-  }
-
-  /* Move the declaration inside &{} to opt into the new behavior */
-  & {
-    margin-bottom: 0;
   }
 }
 

components/projects/projectContent.js
@@ -22,7 +22,7 @@ import classes from './projectContent.module.scss';
  */
 const getCustomRenderers = (currentTheme) => ({
   code({ className, children }) {
-    const language = className.split('-')[1];
+    const language = className?.split('-')[1];
     return (
       <SyntaxHighlighter
         showLineNumbers
@@ -41,21 +41,20 @@ const getCustomRenderers = (currentTheme) => ({
  * @returns {JSX.Element} The rendered Image component.
  */
 const renderImage = (image) => {
-  const isSpecialImage = image === 'portfolio.webp' || image === 'start-page.webp';
-  const width = isSpecialImage ? 850 : 700;
-  const height = isSpecialImage ? 500 : 450;
+  const isSpecialImage = ['portfolio.webp', 'start-page.webp'].includes(image);
+  const dimensions = isSpecialImage ? { width: 850, height: 500 } : { width: 700, height: 450 };
 
   return (
     <Image
       src={`/images/projects/${image}`}
-      width={width}
-      height={height}
+      width={dimensions.width}
+      height={dimensions.height}
       alt=''
       priority
       style={{
         maxWidth: "100%",
         height: "auto",
-        aspectRatio: `${width} / ${height}`
+        aspectRatio: `${dimensions.width} / ${dimensions.height}`
       }} />
   );
 };
@@ -84,7 +83,7 @@ const ProjectContent = ({ project, currentTheme }) => {
   return (
     <div className={classes.projectDetail}>
       <div className='container section mvh-100 projectDetail'>
-        <Link href='/projects/' legacyBehavior>
+        <Link href='/projects/'>
           <motion.button
             whileHover={{ scale: 1.1 }}
             whileTap={{ scale: 0.9 }}

commit ef328f57f0a842717eabf3b879a9ad771b3aa0b8

Fixed broken navbar transition animation (regression from de45373)

components/layout/navbar/navbar.js
@@ -69,20 +69,25 @@ const Navbar = ({ theme, newTheme, children }) => {
 
   return (
     <>
-      <div className={sticky ? `${classes.navbar} ${classes.sticky}` : classes.navbar}>
+      <motion.div
+        className={sticky ? `${classes.navbar} ${classes.sticky}` : classes.navbar}
+        initial={{ y: -100 }}
+        animate={{ y: 0 }}
+        transition={{ duration: 0.5 }}
+      >
         <div className={classes.container}>
-          <Link href='/' passHref>
-            <span className={classes.logo}>
+          <Link href='/' passHref legacyBehavior>
+            <a className={classes.logo}>
               <span className={classes.name}>&lt;</span>/DAVE LEVINE
               <span className={classes.name}>&gt;</span>
-            </span>
+            </a>
           </Link>
 
           <nav className={isOpen ? `${classes.navMenu} ${classes.responsive}` : classes.navMenu} id='navMenu'>
             <div className={classes.linkWrapper}>
               {['/', '/projects', '/certs', '/about'].map((path, index) => (
-                <Link href={path} passHref key={path}>
-                  <motion.span
+                <Link href={path} passHref legacyBehavior key={path}>
+                  <motion.a
                     style={{ cursor: 'pointer' }}
                     initial={{ opacity: 0, y: -100 }}
                     animate={{ opacity: 1, y: 0 }}
@@ -91,7 +96,7 @@ const Navbar = ({ theme, newTheme, children }) => {
                     className={isLinkActive(path) ? classes.activeLink : ''}
                   >
                     {path.toUpperCase().replace('/', '') || 'HOME'}
-                  </motion.span>
+                  </motion.a>
                 </Link>
               ))}
             </div>
@@ -135,7 +140,7 @@ const Navbar = ({ theme, newTheme, children }) => {
             </motion.div>
           </div>
         </div>
-      </div>
+      </motion.div>
       {/* Modal for contact information */}
       <AnimatePresence>
         {showModal && <Modal contact onClose={toggleModal} />}