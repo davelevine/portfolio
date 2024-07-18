4294149c6f1936bba6bb39f69ced441f99a53a94
davelevine <dave@davelevine.io>
2024-07-18 00:00:41 -0400

    Add autocomplete attributes to form fields

--- components/layout/modal/contactModal.js
+++ components/layout/modal/contactModal.js
@@ -106,13 +106,13 @@ const ContactModal = ({ onClose }) => {
             <div className={classes.row}>
               <div className={classes.inputField}>
                 <label htmlFor='email'>Email Address</label>
-                <input id='email' type='email' name='email' required />
+                <input id='email' type='email' name='email' required autocomplete="email" />
 
                 <ValidationError prefix='Email' field='email' errors={state.errors} />
               </div>
             </div>
             <div className={classes.inputField}>
-              <textarea id='message' name='message' required />
+              <textarea id='message' name='message' required autocomplete="message" />
               <ValidationError prefix='Message' field='message' errors={state.errors} />
             </div>
 
