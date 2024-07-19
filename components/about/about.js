351a74cbc3355bf1da8d6a02e0ee7e55eeb67785
davelevine <dave@davelevine.io>
2024-07-19 16:26:42 -0400

    Update about and hero sections
    - Adjusted padding and margins in about.module.scss for better spacing.
    - Updated font sizes and alignments for various screen sizes in about.module.scss.
    - Modified hero.module.scss to improve layout and responsiveness.

--- components/about/about.js
+++ components/about/about.js
@@ -1,12 +1,12 @@
 // Import required modules and components
 import { useState, useEffect, useCallback } from 'react';
-import Image from "next/image";
+import Image from 'next/image';
 import Aos from 'aos'; // Library for scroll animations
 import 'aos/dist/aos.css'; // Styles for AOS animations
 import { AnimatePresence } from 'framer-motion'; // Import AnimatePresence for animations
+import dynamic from 'next/dynamic'; // Import dynamic for code splitting
 
 import classes from './about.module.scss';
-import dynamic from 'next/dynamic'; // Import dynamic for code splitting
 
 // Dynamically import the contact modal for code splitting
 const Modal = dynamic(() => import('../layout/modal/contactModal'), {
@@ -21,9 +21,7 @@ const About = () => {
   // Initialize AOS (Animate on Scroll) with a duration of 550 milliseconds
   useEffect(() => {
     Aos.init({ duration: 550 }); // Initialize scroll animations
-
-    // Update document title
-    document.title = 'Dave Levine - About Me';
+    document.title = 'Dave Levine - About Me'; // Update document title
   }, []);
 
   // Function to hide scrollbar
@@ -44,13 +42,12 @@ const About = () => {
   }, []);
 
   return (
-    <section className={classes.about} id='about'>
+    <section className={classes.about} id="about">
       <div className={classes.container}>
-        <h2 data-aos='slide-right'>ABOUT ME</h2>
+        <h2 data-aos="slide-right">ABOUT ME</h2>
 
         <div className={classes.row}>
-          <div className={classes.columnLeft} data-aos='fade-right'>
-            {/* Text content with data-aos animation attributes */}
+          <div className={classes.columnLeft} data-aos="fade-right">
             <h1>Designing effective solutions, organizing knowledge, and conveying complexity through visual design.</h1>
             <br />
             <p className={classes.h3}>
@@ -63,26 +60,22 @@ const About = () => {
           </div>
 
           <div className={classes.columnRight}>
-            {/* Image container with profile picture using Next.js Image component with preload */}
             <div className={classes.imageContainer}>
               <Image
-                src='https://cdn.levine.io/uploads/portfolio/public/images/avatar.webp'
+                src="https://cdn.levine.io/uploads/portfolio/public/images/avatar.webp"
                 width={500}
                 height={500}
-                alt='profile-pic'
-                data-aos='fade-left'
+                alt="profile-pic"
+                data-aos="fade-left"
                 priority
-                style={{
-                  maxWidth: "100%",
-                  height: "auto"
-                }} />
+                style={{ maxWidth: '100%', height: 'auto' }}
+              />
             </div>
-            <div className={classes.quote} data-aos='fade-right'></div>
+            <div className={classes.quote} data-aos="fade-right"></div>
           </div>
         </div>
       </div>
       <AnimatePresence>
-        {/* Display the modal when showModal is true */}
         {showModal && <Modal contact={modalType === 'contact'} onClose={closeModalHandler} />}
       </AnimatePresence>
     </section>
