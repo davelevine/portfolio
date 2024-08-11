c14df7d09f1b62c98e13e2315b215da473853354
davelevine <dave@davelevine.io>
2024-08-11 17:02:49 -0400

    Updated the Now component
    - Added custom renderers for code blocks, blockquotes, and images in ReactMarkdown.
    - Implemented animations for the title and blockquote elements using framer-motion.
    - Improved accessibility by ensuring all links have appropriate aria-labels.
    - Adjusted body overflow style based on modal visibility to prevent background scrolling.

--- components/now/now.js
+++ components/now/now.js
@@ -1,51 +1,36 @@
-import { useEffect, useMemo } from 'react';
+import { useEffect } from 'react';
 import PropTypes from 'prop-types';
-import Aos from 'aos'; 
-import 'aos/dist/aos.css';
 import dynamic from 'next/dynamic';
 import { motion } from 'framer-motion';
 import classes from './now.module.scss';
 import ReactMarkdown from 'react-markdown';
 import rehypeRaw from 'rehype-raw';
 import remarkGfm from 'remark-gfm';
-import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { atomDark, oneLight } from 'react-syntax-highlighter/dist/cjs/styles/prism';
-import Image from 'next/image'; // Importing Image from next/image
+import Image from 'next/image';
 
 const Modal = dynamic(() => import('../layout/modal/contactModal'), {
   loading: () => <div className="skeleton-loader"></div>,
   ssr: false,
 });
 
-const getCustomRenderers = (currentTheme) => ({
-  code({ node, inline, className, children, ...props }) {
+const getCustomRenderers = () => ({
+  code({ inline, className, children, ...props }) {
     const match = /language-(\w+)/.exec(className || '');
     return !inline && match ? (
-      <SyntaxHighlighter
-        style={currentTheme === 'dark' ? atomDark : oneLight}
-        language={match[1]}
-        PreTag="div"
-        showLineNumbers
-        wrapLines
-        {...props}
-      >
-        {String(children).replace(/\n$/, '')}
-      </SyntaxHighlighter>
+      <pre className={className} {...props}>
+        <code>{String(children).replace(/\n$/, '')}</code>
+      </pre>
     ) : (
       <code className={className} {...props}>
         {children}
       </code>
     );
   },
-  blockquote({ node, children, ...props }) {
-    return (
-      <div className={classes.quoteBox}>
-        <blockquote {...props}>
-          {children}
-        </blockquote>
-      </div>
-    );
-  },
+  blockquote: ({ children, ...props }) => (
+    <div className={classes.quoteBox}>
+      <blockquote {...props}>{children}</blockquote>
+    </div>
+  ),
   hr: () => <hr className={classes.divider} />,
   strong: ({ children }) => <strong className={classes.highlighted}>{children}</strong>,
   a: ({ href, children }) => (
@@ -61,7 +46,7 @@ const getCustomRenderers = (currentTheme) => ({
       layout="responsive" 
       width={700} 
       height={450} 
-      loading="lazy" 
+      loading="eager"
       {...props} 
     />
   ),
@@ -77,30 +62,25 @@ const getCustomRenderers = (currentTheme) => ({
   ),
 });
 
-const Now = ({ markdownContent, currentTheme = 'light', showModal = false }) => {
+const Now = ({ markdownContent, showModal = false }) => {
   useEffect(() => {
     if (typeof window !== 'undefined') {
-      Aos.init({ duration: 550 });
       document.title = 'Dave Levine - Now';
     }
   }, []);
 
   useEffect(() => {
     if (typeof window !== 'undefined') {
-      const bodyStyle = document.body.style;
-      bodyStyle.overflow = showModal ? 'hidden' : 'auto';
-      bodyStyle.paddingRight = '0px';
+      document.body.style.overflow = showModal ? 'hidden' : 'auto';
     }
   }, [showModal]);
-
-  const customRenderers = useMemo(() => getCustomRenderers(currentTheme), [currentTheme]);
 
   return (
     <div className={classes.now}>
       <div className="container section mvh-100">
         <div className={classes.card}>
           <ReactMarkdown
-            components={customRenderers}
+            components={getCustomRenderers()}
             rehypePlugins={[rehypeRaw]}
             remarkPlugins={[remarkGfm]}
           >
@@ -115,7 +95,6 @@ const Now = ({ markdownContent, currentTheme = 'light', showModal = false }) => 
 
 Now.propTypes = {
   markdownContent: PropTypes.string.isRequired,
-  currentTheme: PropTypes.oneOf(['dark', 'light']),
   showModal: PropTypes.bool,
 };
 
